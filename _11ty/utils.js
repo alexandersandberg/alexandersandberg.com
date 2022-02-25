a7a24649cf491adb37ed5b1dda035f50dcc664bd
Alexander Sandberg <hi@alexandersandberg.com>
2022-02-25 11:04:13 +0100

    webp images and other improvements

--- _11ty/utils.js
+++ _11ty/utils.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+
 const contentful = require('contentful');
+const Image = require("@11ty/eleventy-img");
 
 const contentfulClient = contentful.createClient({
 	space: process.env.CTFL_SPACE,
@@ -11,5 +13,30 @@ const postDateFromString = (string) => new Date(string).toLocaleDateString('en-U
 	year: 'numeric'
 })
 
+const imageShortcode = async (
+	src,
+	alt,
+	widths = [null, 400],
+	formats = ["webp", "jpeg"],
+	sizes = "100vw"
+) => {
+	const parsedSrc = src.startsWith("//") ? `https:${src}` : src;
+	const metadata = await Image(parsedSrc, {
+		widths,
+		formats,
+		outputDir: "./_site/img/",
+	});
+
+	const imageAttributes = {
+		alt,
+		sizes,
+		loading: "lazy",
+		decoding: "async",
+	};
+
+	return Image.generateHTML(metadata, imageAttributes);
+}
+
 exports.contentfulClient = contentfulClient;
 exports.postDateFromString = postDateFromString
+exports.imageShortcode = imageShortcode
