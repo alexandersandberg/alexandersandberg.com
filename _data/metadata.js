9329f1f340179e07a3d2bac14beb844ed01dabd1
Alexander Sandberg <hi@alexandersandberg.com>
2022-03-09 20:15:43 +0100

    new metadata data js file

---
+++ _data/metadata.js
@@ -0,0 +1,19 @@
+module.exports = {
+	url: process.env.URL || "http://localhost:8080",
+	title: "Alexander Sandberg",
+	description: "Meta description",
+	language: "en",
+	source: "https://github.com/alexandersandberg/alexandersandberg.com",
+	feed: {
+		path: "/feed.xml",
+		id: "https://alexandersandberg.com/"
+	},
+	author: {
+		name: "Alexander Sandberg",
+		email: "hi@alexandersandberg.com",
+		url: "https://alexandersandberg.com/",
+		twitter: "alexandberg",
+		github: "alexandersandberg",
+		linkedin: "alexandberg"
+	}
+};
