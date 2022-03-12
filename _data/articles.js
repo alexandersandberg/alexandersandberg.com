0edda8e693b81cab5d5f5cce8d02c5e9ef2c580f
Alexander Sandberg <hi@alexandersandberg.com>
2022-03-12 18:35:18 +0100

    add support for custom createdAt post field

--- _data/articles.js
+++ _data/articles.js
@@ -3,13 +3,13 @@ const client = require('../_11ty/utils.js').contentfulClient;
 module.exports = async () => {
 	return client.getEntries({
 		content_type: 'article',
-		order: 'sys.createdAt'
+		order: 'fields.createdAt'
 	}).then((response) => {
 		const articles = response.items
 			.map((article, index) => ({
 				...article.fields,
 				id: index + 1,
-				createdAt: article.sys.createdAt,
+				createdAt: article.fields.createdAt || article.sys.createdAt,
 				...(article.sys.updatedAt != article.sys.createdAt ? { updatedAt: article.sys.updatedAt } : {})
 			}));
 		return articles;
--- _data/posts.js
+++ _data/posts.js
@@ -3,13 +3,13 @@ const client = require('../_11ty/utils.js').contentfulClient;
 module.exports = async () => {
 	return client.getEntries({
 		content_type: 'post',
-		order: 'sys.createdAt'
+		order: 'fields.createdAt'
 	}).then((response) => {
 		const posts = response.items
 			.map((post, index) => ({
 				...post.fields,
 				id: index + 1,
-				createdAt: post.sys.createdAt,
+				createdAt: post.fields.createdAt || post.sys.createdAt,
 				...(post.sys.updatedAt != post.sys.createdAt ? { updatedAt: post.sys.updatedAt } : {})
 			}));
 		return posts;
