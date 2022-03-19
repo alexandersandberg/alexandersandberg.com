8fbbc3be49e011a651535e72a6e9a1a81fb0c75a
Alexander Sandberg <hi@alexandersandberg.com>
2022-03-19 12:07:42 +0100

    lock lesson numbers to ids so they won't change

--- _data/lessons.js
+++ _data/lessons.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const client = require('../_11ty/utils.js').contentfulClient;
 
 module.exports = async () => {
@@ -5,14 +6,33 @@ module.exports = async () => {
 		content_type: 'lesson',
 		order: 'sys.createdAt',
 		limit: 1000,
-	}).then((response) => {
-		const lessons = response.items
-			.map((lesson, index) => ({
-				...lesson.fields,
-				id: index + 1,
-				updatedAt: new Date(lesson.sys.updatedAt),
-				category: lesson.fields.category.fields
-			}));
+	}).then(({ items }) => {
+		const lessonIds = JSON.parse(fs.readFileSync("./_data/lessonIds.json", "utf8"));
+		const newLessonIds = [...lessonIds]
+
+		const lessons = items
+			.map((lesson) => {
+				let lessonId = lesson.sys.id;
+				let numberIndex = newLessonIds.findIndex(id => id === lessonId);
+				var number = numberIndex !== -1 ? numberIndex + 1 : undefined;
+
+				if (!number) {
+					number = newLessonIds.length + 1;
+					newLessonIds.push(lessonId);
+				}
+
+				return ({
+					...lesson.fields,
+					number: number,
+					updatedAt: new Date(lesson.sys.updatedAt),
+					category: lesson.fields.category.fields
+				})
+			});
+
+		if (lessonIds.length !== newLessonIds.length) {
+			fs.writeFileSync("./_data/lessonIds.json", JSON.stringify(newLessonIds, null, "\t"));
+		}
+
 		return lessons;
 	})
 		.catch(console.error);
