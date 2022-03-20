146a319701f5847bd78a8084dd5a1681511f1e13
Alexander Sandberg <hi@alexandersandberg.com>
2022-03-20 13:50:39 +0100

    update page descriptions and meta data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
 	url: process.env.URL || "http://localhost:8080",
 	url_live: "https://alexandersandberg.com/",
-	title: "Alexander Sandberg",
-	description: "Meta description",
+	title: "Alexander Sandberg · Product-minded iOS/macOS Developer",
+	description: "My home on the internet where I share my articles, blog posts, life lessons, and projects as a product-minded iOS/macOS Developer.",
 	language: "en",
 	source: "https://github.com/alexandersandberg/alexandersandberg.com",
 	feed: {
