f218ae54c0e17e4d16c032442e9bf98a2a8e047c
Alexander Sandberg <hi@alexandersandberg.com>
2024-03-15 18:07:55 +0100

    add swift-html dependency

--- Package.swift
+++ Package.swift
@@ -4,9 +4,15 @@ import PackageDescription
 
 let package = Package(
 	name: "Website",
+	dependencies: [
+		.package(url: "https://github.com/binarybirds/swift-html", from: "1.7.0")
+	],
 	targets: [
 		.executableTarget(
-			name: "Website"
+			name: "Website",
+			dependencies: [
+				.product(name: "SwiftHtml", package: "swift-html")
+			]
 		),
 	]
 )
