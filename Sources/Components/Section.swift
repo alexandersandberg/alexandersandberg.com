fba08cab8510436cdbe3c3fa9ace9de43cec9b34
Alexander Sandberg <hi@alexandersandberg.com>
2024-03-22 10:13:05 +0100

    add support for link in section

--- Sources/Components/Section.swift
+++ Sources/Components/Section.swift
@@ -9,15 +9,30 @@ import SwiftHtml
 
 struct Section: TagRepresentable {
 	var title: String
+	var link: (label: String, href: String)?
 	var hiddenTitle: Bool = false
 	var contentClass: String = "v-gap-l"
 	@TagBuilder var content: () -> Tag
 
 	func build() -> Tag {
 		SwiftHtml.Section {
-			H2(title)
-				.class("footnote", "monospace", "tertiary", "uppercase")
-				.class("sr-only", hiddenTitle)
+			Div {
+				H2(title)
+					.class("footnote", "monospace")
+					.class("sr-only", hiddenTitle)
+
+				if let link {
+					P {
+						Span("[ ")
+						A(link.label)
+							.href(link.href)
+						Span(" ]")
+					}
+					.class("footnote", "monospace")
+				}
+			}
+			.class("tertiary", "uppercase")
+			.style("display: flex; justify-content: space-between;")
 
 			Div {
 				content()
