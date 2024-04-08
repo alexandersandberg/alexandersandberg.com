8e5e1c32283e05bb142813cd308cd7ba4f124142
Alexander Sandberg <hi@alexandersandberg.com>
2024-04-08 15:13:29 +0200

    update bio

--- Sources/Main.swift
+++ Sources/Main.swift
@@ -15,7 +15,7 @@ let outputDirectory = URL(fileURLWithPath: "\(rootPath)/.output", isDirectory: t
 enum Site {
 	static let url = "https://alexandersandberg.com"
 	static let author = "Alexander Sandberg"
-	static let bio = "<em>Software tinkerer</em>. Creating, experimenting, learning. Keeping it simple. <span class='nowrap'>Building carefully</span> crafted Swift and SwiftUI apps."
+	static let bio = "<em>Software tinkerer</em>. Creating, exploring, learning. Keeping it simple. <span class='nowrap'>Building carefully</span> crafted Swift and SwiftUI apps."
 	static let email = "hi@alexandersandberg.com"
 	static let cdn = "https://cdn.alexandersandberg.com"
 	static let cssFileName = "styles.css"
