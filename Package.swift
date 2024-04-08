afb14abe64fbe591b4599f901e883b4ed6de0ac1
Alexander Sandberg <hi@alexandersandberg.com>
2024-04-08 15:56:16 +0200

    replace CryptoKit with Crypto

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
 		.macOS(.v14)
 	],
 	dependencies: [
+		.package(url: "https://github.com/apple/swift-crypto.git", from: "3.3.0"),
 		.package(url: "https://github.com/binarybirds/swift-html", from: "1.7.0"),
 		.package(url: "https://github.com/johnsundell/ink", from: "0.6.0")
 	],
@@ -15,6 +16,7 @@ let package = Package(
 		.executableTarget(
 			name: "alexandersandberg.com",
 			dependencies: [
+				.product(name: "Crypto", package: "swift-crypto"),
 				.product(name: "SwiftHtml", package: "swift-html"),
 				.product(name: "SwiftRss", package: "swift-html"),
 				.product(name: "Ink", package: "ink")
