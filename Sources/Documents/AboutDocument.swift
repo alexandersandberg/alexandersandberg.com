b7e250bd25f509a9fadcd37b73ca87c904995651
Alexander Sandberg <hi@alexandersandberg.com>
2024-04-13 10:11:52 +0200

    fix typos in about

--- Sources/Documents/AboutDocument.swift
+++ Sources/Documents/AboutDocument.swift
@@ -12,7 +12,7 @@ let aboutDocument = Document(.html) {
 		Div {
 			P("I’m Alex, a self-taught software engineer born and raised in northern Sweden. I currently live in Austria.")
 			P("I’m very much driven by curiosity. I’ve always loved creating things, exploring new ideas, and seeking new knowledge to learn from.")
-			P("I have a big appreciation for good design and polished experiences, and consider myself a product-minded engineer. I actually have a master’s degree in product design as well.")
+			P("I have a big appreciation for good design and polished experiences and consider myself a product-minded engineer. I actually have a master’s degree in product design as well.")
 			P("I’m open-minded; I embrace my mistakes and learn from them. I avoid prejudices and find opposing views to my own fascinating, not threatening.")
 			P("I’m an extroverted introvert; while I love meeting new people and exploring new places, I recharge my energy at home.")
 			P("I like to keep things simple, both in work and in life. You can usually see this reflected in my creative output.")
@@ -24,7 +24,7 @@ let aboutDocument = Document(.html) {
 
 	Section(title: "What") {
 		Div {
-			P("I love turning ideas into reality—<em>making things</em>. Nowdays, I spend most of my time carefully crafting simple but beautiful apps and experiences with Swift and SwiftUI.")
+			P("I love turning ideas into reality—<em>making things</em>. Nowadays, I spend most of my time carefully crafting simple but beautiful apps and experiences with Swift and SwiftUI.")
 			P(#"I work as a Senior iOS Engineer at <a href="https://www.composer.trade/">Composer</a> where I’m in charge of our iOS app."#)
 			P(#"I’m working on a time tracking app called <a href="/balance/">Balance</a> that helps you balance work and life."#)
 			P(#"I’m also a member of the <a href="https://www.swift.org/website-workgroup/">Swift Website Workgroup</a> where I help guide the evolution of the <a href="https://www.swift.org">Swift.org</a> website."#)
@@ -40,7 +40,7 @@ let aboutDocument = Document(.html) {
 		Div {
 			P("I think I’ve always had a deep curiosity for learning and creating. As long as I can remember, I’ve always found great joy in reading, puzzling, drawing, building with LEGO… anything creative, really.")
 			P("Ever since I wrote my first lines of code back in 2007, when I was 13 years old, most of my creative output has been in the form of software projects—apps, websites, tools.")
-			P("I’ve also always been a perfectionist—for better or worse. In later years, this has made me gravitate toward simplicty in my work, and I’ve come to highly regard the work of people like Dieter Rams and Steve Jobs.")
+			P("I’ve also always been a perfectionist—for better or worse. In later years, this has made me gravitate toward simplicity in my work, and I’ve come to highly regard the work of people like Dieter Rams and Steve Jobs.")
 			P("This desire for keeping things simple might also have come from my Scandinavian roots, where minimalism is more than just a design trend.")
 			P("At the intersection of my need for innovation and simplistic perfectionism, I have discovered this passion for <em>software tinkering</em>.")
 			P(#"On <a href="/someday/">my someday page</a>, you can learn more about my long-term life goals."#)
